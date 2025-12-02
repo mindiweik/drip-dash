dab5fe3caa4d6a4c4dad8fc3d71412417f662498
Mindi Weik <mindiweik@gmail.com>
2025-12-01 22:22:18 -0700

    add initial styling setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', './index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
